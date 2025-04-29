c941b360254530cf495627615acc780053fd2de3
Jawed khelil <jkhelil@redhat.com>
2025-04-29 16:03:08 +0200

    test

--- main.go
+++ main.go
@@ -5,8 +5,20 @@ import (
 )
 
 func main() {
-	// this is no purpose comment and should trigger a review comment
+	// this comment doesnt add any value and should trigger a review comment instead..
 	// fmt.Prinln("hello world")
+
+	x := 42
+	if x == 42 {
+		fmt.Println("The answer is correct.")
+	}
+
+	// Inefficient string concatenation in a loop
+	result := ""
+	for i := 0; i < 5; i++ {
+		result = result + fmt.Sprintf("Item %d, ", i)
+	}
+	fmt.Println(result)
 
 	fmt.Println("echo hello world")
 }
